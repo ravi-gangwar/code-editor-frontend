f5c175402cb24ca15990ef19d9bd9b148e017126
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2025-02-23 00:34:16 +0530

    be uri

--- src/lib/getBackendURI.ts
+++ src/lib/getBackendURI.ts
@@ -2,7 +2,7 @@ const getBackendURI = (environment: "development" | "production") => {
     if(environment === "development"){
         return "http://localhost:5001/api/v1/"
     }
-    return "https://code-editor-backend-cf7c.onrender.com/api/v1/"
+    return "https://code-editor-backend-0pia.onrender.com/api/v1/"
 }
 
 export default getBackendURI;
