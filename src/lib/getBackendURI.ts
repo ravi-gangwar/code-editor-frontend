38f372559aea4510bbb3c9db3732820c62792873
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2025-07-05 09:54:07 +0530

    fixing

--- src/lib/getBackendURI.ts
+++ src/lib/getBackendURI.ts
@@ -3,14 +3,14 @@ const getBackendURI = () => {
     if(process.env.NEXT_ENV === "production"){
         return "http://localhost:5002/api/v1/"
     }
-    return "http://35.193.251.11:5000/api/v1/"
+    return "http://34.28.102.147:5000/api/v1/"
 }
 
 const getSocketURI = () => {
     if(process.env.NEXT_ENV === "production"){
         return "ws://localhost:8080"
     }
-    return "ws://35.193.251.11:8080"
+    return "ws://34.28.102.147:8080"
 }
 
 export { getBackendURI, getSocketURI };
